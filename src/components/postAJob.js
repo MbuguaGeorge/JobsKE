062e53cf83ecd48dd91c538d396eaa40c08405bb
MbuguaGeorge <mbuguag026@gmail.com>
2022-05-21 12:40:04 +0300

    updating job post to send data to API

--- src/components/postAJob.js
+++ src/components/postAJob.js
@@ -2,6 +2,86 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 class PostAJob extends Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            fullTime: false,
+            partTime: false,
+            freelancer: false,
+            intern: false,
+            details: {
+                title: '',
+                type: '',
+                category: '',
+                location: '',
+                description: ''
+            }
+        }
+    };
+
+    submit = (e) => {
+        e.preventDefault()
+        console.log(this.state.details)
+    };
+
+    handleChange = (e) => {
+        let dets = this.state.details
+        dets[e.target.name] = e.target.value
+        this.setState({details: dets})
+    };
+
+    handleChange1 = () => {
+        this.setState({intern: false})
+        this.setState({partTime: false})
+        this.setState({freelancer: false})
+        this.setState({fullTime: !this.state.fullTime})
+        this.setState(prevState => ({
+            details: {
+                ...prevState.details,
+                type: 'Full Time'
+            }
+        }));
+    };
+
+    handleChange2 = () => {
+        this.setState({fullTime: false})
+        this.setState({intern: false})
+        this.setState({freelancer: false})
+        this.setState({partTime: !this.state.partTime})
+        this.setState(prevState => ({
+            details: {
+                ...prevState.details,
+                type: 'Part Time'
+            }
+        }));
+    };
+
+    handleChange3 = () => {
+        this.setState({fullTime: false})
+        this.setState({partTime: false})
+        this.setState({intern: false})
+        this.setState({freelancer: !this.state.freelancer})
+        this.setState(prevState => ({
+            details: {
+                ...prevState.details,
+                type: 'Freelancer'
+            }
+        }));
+    };
+
+    handleChange4 = () => {
+        this.setState({fullTime: false})
+        this.setState({partTime: false})
+        this.setState({freelancer: false})
+        this.setState({intern: !this.state.intern})
+        this.setState(prevState => ({
+            details: {
+                ...prevState.details,
+                type: 'Internship'
+            }
+        }));
+    };
+
     render(){
         return(
             <>
@@ -10,42 +90,61 @@ class PostAJob extends Component{
                 </div>
                 <div className='post'>
                     <h1>Post a job</h1>
-                    <form>
+                    <form onSubmit={this.submit} >
                         <ul>
                             <li>
                                 <label>Job Title</label>
                                 <input type='text' required 
                                     placeholder='eg. Professional UI Designer '
-                                />
-                            </li>
-                            <li>
-                                <label>Company</label>
-                                <input type='text' required 
-                                    placeholder='eg. Microsoft, ADC'
+                                    name='title'
+                                    value={this.state.details.title}
+                                    onChange={this.handleChange}
                                 />
                             </li>
                             <li>
                                 <h2>Job Type</h2>
-                                <span><input type='radio' required />Full Time</span>
-                                <span><input type='radio' required />Part Time</span>
-                                <span><input type='radio' required />Freelance</span>
-                                <span><input type='radio' required />Internship</span>
+                                <span><input type='radio' required 
+                                    checked={this.state.fullTime}
+                                    onChange={this.handleChange1}
+                                />Full Time</span>
+                                <span><input type='radio' required 
+                                    checked={this.state.partTime}
+                                    onChange={this.handleChange2}
+                                />Part Time</span>
+                                <span><input type='radio' required 
+                                    checked={this.state.freelancer}
+                                    onChange={this.handleChange3}
+                                />Freelance</span>
+                                <span><input type='radio' required 
+                                    checked={this.state.intern}
+                                    onChange={this.handleChange4}
+                                />Internship</span>
                             </li>
                             <li>
                                 <h2>Location</h2>
                                 <input type='text' required 
                                     placeholder='eg. Nairobi'
+                                    name='location'
+                                    value={this.state.details.location}
+                                    onChange={this.handleChange}
                                 />
                             </li>
                             <li>
                                 <h2>Category</h2>
                                 <input type='text' required 
                                     placeholder='eg. Software'
+                                    name='category'
+                                    value={this.state.details.category}
+                                    onChange={this.handleChange}
                                 />
                             </li>
                             <li>
                                 <h2>Description</h2>
-                                <textarea/>
+                                <textarea required
+                                    name='description'
+                                    value={this.state.details.description}
+                                    onChange={this.handleChange}
+                                />
                             </li>
                         </ul>
                         <input type='submit' 
